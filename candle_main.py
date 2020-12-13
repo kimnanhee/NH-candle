365e2180b2010b79e5f640ebc33e4a97b11ad58a
kimnanhee <kimnanhee0225@gmail.com>
2020-12-13 23:16:01 +0900

    [Modify] candle_main.py signals function

--- candle_main.py
+++ candle_main.py
@@ -1,11 +1,20 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from candle import *
 
+def signals(self):
+    self.pushButton_value.clicked.connect(lambda : self.stackedWidget.setCurrentIndex(0))
+    self.pushButton_set.clicked.connect(lambda : self.stackedWidget.setCurrentIndex(1))
+    self.pushButton_graph.clicked.connect(lambda : self.stackedWidget.setCurrentIndex(2))
+
+Ui_MainWindow.signals = signals
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    ui.signals()
+
     MainWindow.show()
     sys.exit(app.exec_())
